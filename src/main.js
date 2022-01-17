2aaa98d38d3a1682b2372a5d277d9407759246e4
hidari123 <60548379+hidari123@users.noreply.github.com>
2022-01-18 00:38:27 +0800

    hidari_shop

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入axios
+import axios from 'axios'
 
 Vue.config.productionTip = false
+// 配置请求的根路径
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 new Vue({
   router,
